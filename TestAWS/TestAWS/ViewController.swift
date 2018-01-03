37d0806528d11747922e1670fc08a579b1de1677
mayulu <mayulu@gmail.com>
2018-01-03 14:41:23 +0800

    Test AWS SDK

---
+++ TestAWS/TestAWS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestAWS
+//
+//  Created by Ma Yulu on 2018/1/3.
+//  Copyright © 2018年 Ma Yulu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
